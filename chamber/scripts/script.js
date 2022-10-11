5ef7eda79381b953e449ead53787a1e1b77e4ed0
Angeline127 <angenyepen@gmail.com>
2022-10-11 04:02:29 -0700

    cc

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,14 +1,18 @@
 const currentDate = new Intl.DateTimeFormat("en-UK", {
-    dateStyle: "full",
-  }).format(new Date());
-  
-  document.querySelector(".date").innerHTML = currentDate;
-  
-  // Hamburger button toggle menu
-  function toggleMenu() {
-    document.querySelector(".nav-info").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-  }
-  
-  const menuButton = document.getElementById("hamburgerBtn");
-  menuButton.onclick = toggleMenu;
+  dateStyle: "full",
+}).format(new Date());
+
+document.querySelector(".date").innerHTML = currentDate;
+
+// Hamburger button toggle menu
+function toggleMenu() {
+  document.querySelector("#nav-info").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const menuButton = document.getElementById("hamburgerBtn");
+menuButton.onclick = toggleMenu;
+
+const date = new Date(document.lastModified).toLocaleString();
+
+document.querySelector("#last-updated").innerHTML = date;
