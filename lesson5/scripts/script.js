17b91fecf94cf23aca08fd5a427a03ed95372b63
Angeline127 <angenyepen@gmail.com>
2022-10-19 18:29:43 +0000

    adding functions

--- lesson5/scripts/script.js
+++ lesson5/scripts/script.js
@@ -3,10 +3,22 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    const myItem = input.value;
-    input.value = '';
+const myItem = input.value;
+input.value = '';
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
-}
+const listItem = document.createElement('li');
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+
+listItem.appendChild(listText);
+listText.textContent = myItem;
+listItem.appendChild(listBtn);
+listBtn.textContent = '❌';
+list.appendChild(listItem);
+
+listBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
+});
+
+input.focus();
+});
