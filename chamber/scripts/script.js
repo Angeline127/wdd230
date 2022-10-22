c30ae42051684b77144fd5e99993e8084449b40f
Angeline127 <angenyepen@gmail.com>
2022-10-22 10:52:50 +0000

    join us

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -16,3 +16,13 @@ menuButton.onclick = toggleMenu;
 const date = new Date(document.lastModified).toLocaleString();
 
 document.querySelector("#last-updated").innerHTML = date;
+
+
+//greet us//
+const day = new Date();
+const today = day.getDay();
+
+if (today < 1 || today > 2) {
+  const banner = document.querySelector('.join-us');
+  banner.remove();
+}
