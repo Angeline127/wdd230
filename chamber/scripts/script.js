2142eeae2fa36c3285ecebfc7fa969658d93d3f8
Angeline127 <angenyepen@gmail.com>
2022-11-05 22:09:43 +0000

    discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -59,3 +59,23 @@ if ("IntersectionObserver" in window) {
     loadImages(img);
   });
 }
+
+//Day Before//
+let dayBefore = localStorage.getItem('date');
+
+localStorage.setItem('date', day);
+
+dayBefore = Date.parse(dayBefore);
+
+const number_mSdayS = (day - dayBefore)/86400000;
+
+
+if (!number_mSdayS) {
+    const numDaysN = document.querySelector("#lastVisit");
+    if (numDaysN) { numDaysN.textContent = 0; }
+}
+
+else {
+    const numDaysY = document.querySelector("#lastVisit");
+    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
+}
