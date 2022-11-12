e2ad90b5e794a3826cba23ed19f10f18b2f6408c
Angeline127 <angenyepen@gmail.com>
2022-11-12 21:37:57 +0000

    directory

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -37,7 +37,7 @@ const loadImages = (image) => {
   };
 };
 
-const imgOptions= {
+const imgOptions = {
   threshold: 0.5,
   rootMargin: "0px 0px 50px 0px"
 };
@@ -60,22 +60,94 @@ if ("IntersectionObserver" in window) {
   });
 }
 
-//Day Before//
+//Last Visit//
 let dayBefore = localStorage.getItem('date');
 
 localStorage.setItem('date', day);
 
 dayBefore = Date.parse(dayBefore);
 
-const number_mSdayS = (day - dayBefore)/86400000;
+const number_mSdayS = (day - dayBefore) / 86400000;
 
 
 if (!number_mSdayS) {
-    const numDaysN = document.querySelector("#lastVisit");
-    if (numDaysN) { numDaysN.textContent = 0; }
+  const numDaysN = document.querySelector("#lastVisit");
+  if (numDaysN) { numDaysN.textContent = 0; }
 }
 
 else {
-    const numDaysY = document.querySelector("#lastVisit");
-    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
+  const numDaysY = document.querySelector("#lastVisit");
+  if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
 }
+//Thank You //
+
+//Directory //
+const cards = document.querySelector('.cards');
+
+fetch('./json/data.json')
+  .then((response) => response.json())
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const directory = jsonObject['directory'];
+    directory.forEach((dir) => displayDirectory(dir));
+  });
+
+function displayDirectory(dir) {
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  let image = document.createElement('img');
+  let h2 = document.createElement('h2');
+  let address = document.createElement('p');
+  let phoneNumber = document.createElement('p');
+  let website = document.createElement('a');
+
+  // Change the textContent property of the h2 element
+  if (h2) h2.textContent = dir.name;
+  if (address) address.textContent = ` ${dir.address}`;
+  if (phoneNumber) phoneNumber.textContent = ` ${dir.phoneNumber}`;
+  if (website) {
+    website.textContent = dir.website;
+    website.href = dir.website;
+  }
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  image.setAttribute('src', dir.imgUrl);
+  image.setAttribute('alt', `${dir.name}`);
+  image.setAttribute('loading', 'lazy');
+
+  // Add/append the section(card) with the h2 element
+  card.appendChild(h2);
+  card.appendChild(image);
+  card.appendChild(address);
+  card.appendChild(phoneNumber);
+  card.appendChild(website);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  const divGrid = document.querySelector('div.grid');
+  if (divGrid) divGrid.appendChild(card);
+}
+
+//Grid //
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('#business');
+
+if (gridbutton) {
+  gridbutton.addEventListener('click', () => {
+    // example using arrow function
+    if (display) {
+      display.classList.add('grid');
+      display.classList.remove('list');
+    }
+  });
+}
+
+if (listbutton) listbutton.addEventListener('click', showList);
+
+function showList() {
+  if (display) {
+    display.classList.add('list');
+    display.classList.remove('grid');
+  }
+}
+
