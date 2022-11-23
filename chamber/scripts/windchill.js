0f87b94b498bfece24989f58084a10dea28137d2
Angeline127 <angenyepen@gmail.com>
2022-11-23 10:49:44 +0000

    edits

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -17,41 +17,45 @@
 
 
 // select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-const winds = document.querySelector('wind');
+const currentTemp = document.querySelector('.temperature');
+const weatherIcon = document.querySelector('.weather-icon');
+const captionDesc = document.querySelector('.cloud');
+const windSpeed = document.querySelector('.wind-speed');
+
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kumasi&units=imperial&appid=4831de6057d4fe27a81d6dd35aae65da';
+
 
 async function apiFetch() {
     try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data); // this is for testing the call
-            displayResults(data);
-        }else {
-            throw Error(await response.text());
-        }
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
     } catch (error) {
         console.log(error);
     }
 }
-
+  
 apiFetch();
 
-
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-}
-
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+function displayResults(weatherData) {    
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>&deg;F`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
-
+  
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
+
+    windSpeed.innerHTML = `Wind Speed: <strong>${weatherData.wind.speed.toFixed(0)}</strong> mph`;
 }
+
+// windchill 
+var windChill = (35.74 + (0.6215 * currentTemp)) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
+var windChill = Math.round(windChill);
+document.querySelector('.windchill').textContent = windChill;
